bad3e374e59ae6ebbd8b01ff7a730e365d547e89
hamsyed <40880601+hamsyed@users.noreply.github.com>
2019-12-10 16:39:07 +1100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 Public Class HelloWorld{
   Public static void main(String [] Args) {
 
-    System.out.println(">>>>>>>>>>>>>>This is Hello World<<<<<<<<<<<<");
+    System.out.println(">>>>>>>>>>>>>>This is Jenkins World<<<<<<<<<<<<");
 
 }
 }
