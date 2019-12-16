510a23f8740c4aa753e1eab3e46923ddcde31869
hamsyed <40880601+hamsyed@users.noreply.github.com>
2019-12-16 14:15:05 +1100

    updated the spaces

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ Public Class HelloWorld{
   Public static void main(String [] Args) {
 
     System.out.println(">>>>>>>>>>>>>>This is Hello World<<<<<<<<<<<<");
-      System.out.println(">>>>>>>>>>>>>>This is Jenkins World<<<<<<<<<<<<");
+    System.out.println(">>>>>>>>>>>>>>This is Jenkins World<<<<<<<<<<<<");
 
 }
 }
